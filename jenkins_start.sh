27b294502a0c750f1713b5c1cd852dd96df8f81c
Dorian Beganovic <beganovic.dorian@gmail.com>
2018-06-10 21:17:27 +0200

    fixed a typo

--- jenkins_start.sh
+++ jenkins_start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-docker run
---rm
--u root
--p 8080:8080
--v jenkins-data:/var/jenkins_home
--v /var/run/docker.sock:/var/run/docker.sock
--v "$HOME":/home
---name jenkins-tutorials
+docker run \
+--rm \
+-u root \
+-p 8080:8080 \
+-v jenkins-data:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v "$HOME":/home \
+--name jenkins-tutorials \
 jenkinsci/blueocean
